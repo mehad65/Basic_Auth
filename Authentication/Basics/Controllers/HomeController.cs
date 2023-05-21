92986e92b02f45769c55e0c356491718bc83d466
Mihadul Islam <mehad65@gmail.com>
2023-05-22 00:32:05 +0600

    Basic auth first part done

---
+++ Authentication/Basics/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Mihadul"),
+                new Claim(ClaimTypes.Email, "mihad@gmail.com"),
+                new Claim("Grandma.Says", "Very nice boy")
+            };
+
+            var licenseClaims = new List<Claim>()
+            {
+
+                new Claim(ClaimTypes.Name, "Mihadul Islam"),
+                new Claim("DrivingLicense", "A+")
+            };
+
+            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "License Identity");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
